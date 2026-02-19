a85f53e835beb219329aa883c5f59e1355a03b04
DoctorEther <68768464+DoctorEther@users.noreply.github.com>
2026-02-19 19:14:11 -0300

    Feijoada tweaks

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 C:\fteqcc\fteqcc64.exe -O1 -std=qccx src\progs.src
 
-del /q "C:\Users\%USERNAME%\Desktop\bddb4\id1\progs.dat"
-move "progs.dat" "C:\Users\%USERNAME%\Desktop\bddb4\id1\"
+del /q "%USERPROFILE%\Desktop\bddb4\id1\progs.dat"
+move "progs.dat" "%USERPROFILE%\Desktop\bddb4\id1\"
 
 pause
