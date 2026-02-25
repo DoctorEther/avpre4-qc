5a55a79d7e44ff72606e2ca44ef43366dfe7a6a1
DoctorEther <68768464+DoctorEther@users.noreply.github.com>
2026-02-25 18:37:30 -0300

    New triggers: trigger_ladder, trigger_cdtrack & trigger_soundplayer

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-C:\fteqcc\fteqcc64.exe -O1 -std=qccx src\progs.src
+C:\Quake\fteqcc\fteqcc64.exe -O1 -std=qccx src\progs.src
 
 del /q "%USERPROFILE%\Desktop\bddb4\id1\progs.dat"
 move "progs.dat" "%USERPROFILE%\Desktop\bddb4\id1\"
