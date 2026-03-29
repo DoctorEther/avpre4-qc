8680b6e0d070fff9b7dcd857d11071c532f1c970
DoctorEther <68768464+DoctorEther@users.noreply.github.com>
2026-03-29 09:18:18 -0300

    Added skating movement

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 
+set "progs_path=%USERPROFILE%\Desktop\bddb4\bddpre4"
+
 C:\Quake\fteqcc\fteqcc64.exe -O1 -std=qccx src\progs.src
 
-del /q "%USERPROFILE%\Desktop\bddb4\id1\progs.dat"
-move "progs.dat" "%USERPROFILE%\Desktop\bddb4\id1\"
+if exist "progs.dat" move /Y "progs.dat" "%progs_path%" >nul
 
 pause
